1a14fb531c8f8260efd61c6893564e724c47c892
vinayciba <47775023+vinayciba@users.noreply.github.com>
2020-06-04 11:23:25 +0530

    Modified to work with a single script

--- run-mrf-wrapper.sh
+++ run-mrf-wrapper.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 
-MUM_LEN=20 
-OUTPUT_PREFIX=pre
-usage() 
+MUM_LEN=20
+CDS_PROP_THRESH=0
+FALSE_MATCH_THRESH=15
+NEGOFF=1
+POSOFF=1
+OUTPUT_PREFIX=mrfOUT
+usage()
 
 {
 echo "Usage:	$0 -q query fasta -r reference fasta -f reference gff3
 
 options: -m	 mummer exact match length [default: 20]
-	 -o 	 output prefix [default: pre]
+	 -l 	 False match length threshold.Values below this will be screened and confirmed if they are true matches [default: 15]
+	 -n 	 Negative offset. This option requires False match length as mandatory argument [default: 1]
+	 -p      Positive offset. This option requires False match length as mandatory argument [default: 1]
+	 -c 	 Partial coding sequences below this set threshold are not shown [default: 0]
+	 -o 	 output prefix [default: mrfOUT]
 	 -h 	 prints usage
 	 " 1>&2; exit 1;
 }
 
-while getopts ":m:g:q:r:f:o:" name;
+while getopts ":m:g:q:r:f:l:n:p:c:o:" name;
    do
      case "${name}" in
-        m) MUM_LEN=${OPTARG} ;;           # mummer mum length
-        g) GENOME_SIZE=${OPTARG} ;;       # genome size
-        q) QUERY=${OPTARG} ;;     	  # query genome
-        r) REFERENCE=${OPTARG} ;;         # reference genome
-        f) GFF3_FILE=${OPTARG} ;;         # gff3 file
-	o) OUTPUT_PREFIX=${OPTARG} ;;	  # prefix for the output file name
-        *) usage ;;                       # display usage and exit
+        m) MUM_LEN=${OPTARG} ;;           	# mummer mum length
+        g) GENOME_SIZE=${OPTARG} ;;       	# genome size
+        q) QUERY=${OPTARG} ;;     	  	# query genome
+        r) REFERENCE=${OPTARG} ;;         	# reference genome
+        f) GFF3_FILE=${OPTARG} ;;         	# gff3 file
+	l) FALSE_MATCH_THRESH=${OPTARG} ;;	# False match length cutoff value
+	n) NEGOFF=${OPTARG} ;; 			# Negative Offset
+	p) POSOFF=${OPTARG} ;;			# Positive offset
+	c) CDS_PROP_THRESH=${OPTARG} ;;		# CDS below this percentage will not be shown
+	o) OUTPUT_PREFIX=${OPTARG} ;;	  	# prefix for the output file name
+        *) usage ;;                       	# display usage and exit
      esac
    done
 shift $((OPTIND-1))
@@ -32,19 +44,29 @@ if [ -z "${QUERY}" ] || [ -z "${REFERENCE}" ] ; then
 fi
 if [ ! -z "${QUERY}" ] & [ ! -z "${REFERENCE}" ] & [ -z "${GFF3_FILE}" ]; then
 
+
 echo "GFF3 file not provided, so proceeding to generate only missing genomic regions. Please provide reference GFF3 file to generate missing coding sequences."
+#echo $'\n';
 echo "MUM_LEN = ${MUM_LEN}"
 echo "QUERY = ${QUERY}"
 echo "REFERENCE = ${REFERENCE}"
 echo "GFF3_FILE = ${GFF3_FILE}"
+echo "FALSE_MATCH_THRESH = $[FALSE_MATCH_THRESH]"
+echo "NEGOFF = $[NEGOFF]"
+echo "POSOFF = $[POSOFF]"
+echo "CDS_PROP_THRESH = $[CDS_PROP_THRESH]"
+echo $'\n';
 
-mummer -mum -l $MUM_LEN $REFERENCE $QUERY > mum_new_out;
-awk '{ for(i=1;i<=NF;i++){if(i==NF){printf("%s\n",$NF);}else {printf("%s\t",$i)}}}' mum_new_out |  awk 'NR > 1' > mum_new_formatted;
+mummer -mum -l $MUM_LEN $REFERENCE $QUERY > $OUTPUT_PREFIX.mum;
 
 ref_gen_len=`awk 'NR>1{s+=length()}END{print s}' $REFERENCE`;
-#echo $'\n';
+
+
 echo "Generating missing genomic regions..."
-perl generate_missing_genomic_regions.pl mum_new_formatted $ref_gen_len $OUTPUT_PREFIX;
+
+perl missing_regions_finder.pl --inputfile $OUTPUT_PREFIX.mum --genome_size $ref_gen_len;
+
+echo $'\n';
 echo "done"
 
 else
@@ -54,24 +76,26 @@ echo "MUM_LEN = ${MUM_LEN}"
 echo "QUERY = ${QUERY}"
 echo "REFERENCE = ${REFERENCE}"
 echo "GFF3_FILE = ${GFF3_FILE}"
+echo "FALSE_MATCH_THRESH = $[FALSE_MATCH_THRESH]"
+echo "NEGOFF = $[NEGOFF]"
+echo "POSOFF = $[POSOFF]"
+echo "CDS_PROP_THRESH = $[CDS_PROP_THRESH]"
 
-
-mummer -mum -l $MUM_LEN $REFERENCE $QUERY > mum_new_out;
-
-awk '{ for(i=1;i<=NF;i++){if(i==NF){printf("%s\n",$NF);}else {printf("%s\t",$i)}}}' mum_new_out |  awk 'NR > 1' > mum_new_formatted;
+echo $'\n';
+mkdir circos_files;
+mummer -mum -l $MUM_LEN $REFERENCE $QUERY > $OUTPUT_PREFIX.mum;
 
 ref_gen_len=`awk 'NR>1{s+=length()}END{print s}' $REFERENCE`;
 
-#echo $ref_gen_len
-#echo $'\n';
+echo $'\n';
 
 echo "Generating missing regions and missing coding regions..."
-perl generate_missing_genomic_regions.pl mum_new_formatted $ref_gen_len $OUTPUT_PREFIX;
-perl generate_missing_coding_sequences.pl mum_new_formatted $GFF3_FILE $ref_gen_len $OUTPUT_PREFIX;
+
+echo $'\n';
+
+perl missing_regions_finder.pl --inputfile  $OUTPUT_PREFIX.mum --genome_size $ref_gen_len --gff3 $GFF3_FILE --cds_threshold $CDS_PROP_THRESH --fmatch_len $FALSE_MATCH_THRESH --negoff $NEGOFF --posoff $POSOFF;
 echo "done"
 
 fi
 
 
-rm mum_new_out mum_new_formatted
-rm featuretable.gff3
